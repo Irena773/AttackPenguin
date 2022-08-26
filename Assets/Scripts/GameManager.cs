357bc168fd4637027fd81c6fc0457b13f4fcf467
Irena773 <irina0773555666@gmail.com>
2022-08-26 17:52:01 +0900

    制限時間を実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,22 +6,32 @@ using UnityEngine.UI;
 //ゲームの進行管理や全体的なパラメーターを保持する
 public class GameManager : MonoBehaviour
 {
-
     //プレイヤーのスコア
     private int score;
     private Text scoreText;
     //加算するポイント
     private int addPoint = 1;
 
+    //制限時間
+    private float limitTime = 60;
+    private Text timeText;   
+
     void Start()
     {
         score = 0;
         scoreText = GameObject.Find("Score").GetComponent<Text>();
+        timeText  = GameObject.Find("Time").GetComponent<Text>();
     }
 
     void Update()
     {
         scoreText.text = score.ToString();
+        if(0 < limitTime)
+        {
+            limitTime -= Time.deltaTime;
+            timeText.text = limitTime.ToString("F0");
+        }
+        
     }
 
     public void AddPoint()
